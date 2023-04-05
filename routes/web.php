89c81eedf4bfa3fb1a9f7c0eb2b8bf921da181d7
nekoze27 <teketeke.jimmy@gmail.com>
2023-04-05 08:32:45 +0000

    PHP/Laravel12 kadai

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 // 3
@@ -31,8 +31,8 @@ Route::controller(AAAController::class)->group(function() {
 // 4
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->group(function() {
-    Route::get('admin/profile/create','add');
-    Route::get('admin/profile/edit','edit');
+    Route::get('admin/profile/create','add')->middleware('auth');
+    Route::get('admin/profile/edit','edit')->middleware('auth');
 });
 Auth::routes();
 
