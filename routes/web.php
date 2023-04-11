8455b08d5ad5234df4d0fedceabc0ae5bd4debc7
nekoze27 <teketeke.jimmy@gmail.com>
2023-04-11 07:50:05 +0000

    PHP/Laravel13 kadai

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,14 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
+/*
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add')->middleware('auth');
+});
+*/
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
 // 3
@@ -30,9 +36,11 @@ Route::controller(AAAController::class)->group(function() {
 
 // 4
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->group(function() {
-    Route::get('admin/profile/create','add')->middleware('auth');
-    Route::get('admin/profile/edit','edit')->middleware('auth');
+Route::controller(ProfileController::class)->name('admin.')->middleware('auth')->group(function() {
+    Route::get('admin/profile/create','add')->name('profile.add');
+    Route::post('admin/profile/create','create')->name('profile.create');
+    Route::get('admin/profile/edit','edit')->name('profile.add');
+    Route::post('admin/profile/edit','update')->name('profile.edit');
 });
 Auth::routes();
 
